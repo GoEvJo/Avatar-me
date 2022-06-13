effad1d650907b0b643fe1443f21b5e8a2f7083a
Joaquín G <104398196+GoEvJo@users.noreply.github.com>
2022-06-13 10:36:44 -0300

    Update errorM.go

--- avatar/errorMessages/errorM.go
+++ avatar/errorMessages/errorM.go
@@ -7,6 +7,5 @@ import (
 var (
 	NoFile     = errors.New("error with the file")
 	Hashing    = errors.New("incorrect hash value")
-	BadLength  = errors.New("non-positive length")
 	NullString = errors.New("empty string")
 )
