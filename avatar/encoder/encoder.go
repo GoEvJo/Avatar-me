b42fbdef6696bad9582b6c70642de656a30c17b4
Joaquín G <104398196+GoEvJo@users.noreply.github.com>
2022-06-13 10:35:10 -0300

    Update encoder.go

--- avatar/encoder/encoder.go
+++ avatar/encoder/encoder.go
@@ -6,32 +6,29 @@ import (
 	"github.com/GoEvJo/Avatar-me/avatar/errorMessages"
 )
 
-// myEncoder is a struct wich contains the information (not-null string) to be hashed.
 type myEncoder struct {
-	strInformation string
+}
+
+// Function that provides a pointer to a new myEncoder struct capable of using the interface.
+func NewMyEncoder() myEncoder {
+	NewEncoder := myEncoder{}
+	return NewEncoder
 }
 
 // EncodeInformation is the function in charge of making the hash from a string. It returns hashed info (Sha512) and an error, if it occurs.
-func (e *myEncoder) EncodeInformation() (encodedInformation []byte, err error) {
-	//init the hash
+func (e *myEncoder) EncodeInformation(strInformation string) (encodedInformation []byte, err error) {
+
+	if strInformation == "" {
+		return nil, errorMessages.NullString
+	}
+
 	sha512 := sha512.New()
 
-	//pass the string
-	sha512.Write([]byte(e.strInformation))
-
-	//64bit hash code
+	sha512.Write([]byte(strInformation))
 	returningValue := sha512.Sum(nil)
 	if len(returningValue) != 64 {
 		err = errorMessages.Hashing
 		return nil, err
 	}
 	return returningValue, nil
-}
-
-// Function that provides a pointer to a new myEncoder struct from a string.
-func NewMyEncoder(strInformation string) *myEncoder {
-	NewEncoder := myEncoder{
-		strInformation: strInformation,
-	}
-	return &NewEncoder
 }
